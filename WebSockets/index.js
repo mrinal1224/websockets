8ef27416973f1da958bfe8d151915642a679b640
mrinal1224 <iammrinalkc@gmail.com>
2025-12-04 22:01:11 +0530

    push

--- WebSockets/index.js
+++ WebSockets/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const path = require('path')
-
+const http = require('http')
+const {Server} = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server)
+
+
+
+io.on('connection' , (socket)=>{
+    console.log('Client Connection Successful' , socket.id)
+})
+
+
 
 app.use(express.static(path.resolve('./public')))
 
@@ -13,6 +25,6 @@ app.get('/' , (req , res)=>{
 
 
 
-app.listen(8005 , ()=>{
+server.listen(8005 , ()=>{
     console.log("Server Started")
 })
